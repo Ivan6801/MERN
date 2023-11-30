85c39091d4a9ca4f8b093a1ab970a4396fc36898
Ivan Gonzalez <hal308366@gmail.com>
2023-11-29 18:09:01 -0800

    Conectar la App a la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import conectarDB from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
 app.listen(4000, () => {
-  console.log("Servidor corriendo en el puerto 4000");
+  console.log(`Servidor corriendo en el puerto localhost:${PORT}`);
 });
