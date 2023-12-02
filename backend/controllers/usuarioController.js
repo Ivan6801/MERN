7e0b93a38ca1e59539516edb70419a0e6df7a72c
Ivan Gonzalez <hal308366@gmail.com>
2023-12-01 23:45:48 -0800

    Validaciones para Autenticacion

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -22,4 +22,23 @@ const registrar = async (req, res) => {
   }
 };
 
-export { registrar };
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Comprobar si el usuario existe
+  const usuario = await Usuarios.findOne({ email });
+
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+  // Comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmado");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Comprobar su password
+};
+
+export { registrar, autenticar };
