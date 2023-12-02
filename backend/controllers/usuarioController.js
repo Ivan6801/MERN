78dde42ec4157fb411606455ab1f12f0c1d2a2ae
Ivan Gonzalez <hal308366@gmail.com>
2023-12-01 19:12:07 -0800

    Evitar usuarios duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,6 +1,16 @@
 import Usuarios from "../models/Usuario.js";
 
 const registrar = async (req, res) => {
+  // Evitar registros duplicados
+
+  const { email } = req.body;
+  const existeUsuario = await Usuarios.findOne({ email });
+
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const usuario = new Usuarios(req.body);
     const usuarioAlmacenado = await usuario.save();
