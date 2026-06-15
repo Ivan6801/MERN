13a30b30910120dfb09a51d0a124bd87b51ec98e
Ivan Gonzalez <igonzalez@hospitalexcel.com>
2026-06-15 15:07:40 -0700

    Create frontend

--- frontend/src/paginas/Proyectos/index.jsx
+++ frontend/src/paginas/Proyectos/index.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import useProyectos from "../../hooks/useProyectos"
+import PreviewProyecto from "../../components/PreviewProyecto"
+import { Alerta } from "../../components/Alerta"
 
 export default function Proyectos() {
+  const { proyectos, alerta } = useProyectos();
+  const { msg } = alerta;
+
   return (
-    <div>
-      <h1>Proyectos</h1>
-    </div>
+    <>
+      <h1 className="text-4xl font-black">Proyectos</h1>
+      {msg && <Alerta alerta={alerta} />}
+
+      <div className="bg-white shadow mt-10 rounded-lg ">
+        {proyectos.length ?
+          proyectos.map((proyecto) => (
+            <PreviewProyecto
+              key={proyecto._id}
+              proyecto={proyecto}
+            />
+          ))
+          : <p className=" text-center text-gray-600 uppercase  p-5">No hay proyectos aún</p>}
+      </div>
+    </>
   );
 }
